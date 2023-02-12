3422625ed2b57507ca9e2b874eabf1250f54754d
Konstantin Maslov <maslovkv@mail.ru>
2023-02-12 10:43:48 +0300

    Домашнее 5.5

---
+++ 5/5.5/src/terraform/variables.tf
@@ -0,0 +1,17 @@
+# Заменить на ID своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_cloud_id" {
+  default = "b1g8isf6e7r1t01j6ejk"
+}
+
+# Заменить на Folder своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_folder_id" {
+  default = "b1g13isi9ot5i1j4f1r0"
+}
+
+# Заменить на ID своего образа
+# ID можно узнать с помощью команды yc compute image list
+variable "centos-7-base" {
+  default = "fd86j22q4mhdjae507er"
+}
